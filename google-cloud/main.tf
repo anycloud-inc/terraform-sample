3ca216019017ffabd728b896f844979e1ab509a7
KazutakaShimizu <s.kazutaka55555@gmail.com>
2022-03-08 19:20:29 +0900

    initial commit

---
+++ google-cloud/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  # 任意のプロジェクトに変更
+  project     = "test-terraform-343107"
+  region      = "asia-northeast1"
+  # 配置したサービスアカウントキーのパスを指定
+  credentials = file("service-account.json")
+}
+
+resource "google_sql_database_instance" "sample" {
+  # 任意の名前に変更
+  name             = "sample-instance"
+  database_version = "MYSQL_8_0"
+  region      = "asia-northeast1"
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
